0d98d29d9ff0f40c00a8e2bbdc8a0e5e481af3b6
wocodlol <80688481+wocodlol@users.noreply.github.com>
2021-03-15 13:47:18 +0100

    Add files via upload

---
+++ src/store/model/emerita.ts
@@ -0,0 +1,155 @@
+import {Store} from './store';
+
+export const Emerita: Store = {
+  currency: '€',
+  labels: {
+    inStock: {
+      container: '.availability',
+      text: ['disponible'],
+    },
+    maxPrice: {
+      container: '.text-price-total',
+      euroFormat: true,
+    },
+    outOfStock: {
+      container: '.stock out-of-stock',
+      text: ['Agotado'],
+    },
+  },
+  links: [
+    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-pny-nvidia-geforce-rtx-3080-10gb-xlr8-gaming-uprising-epic-x-rgb-gddr6x-display-port-hdmi-triple-ventilador/',
+    },
+	{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-pny-nvidia-geforce-rtx-3080-10gb-xlr8-gaming-revel-epic-x-rgb-gddr6x-display-port-hdmi-triple-ventilador/',
+    },
+	    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-gigabyte-gaming-nvidia-rtx3080-oc10g-10gb-gddr6x-hdmi-display-port/',
+    },
+	{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-gigabyte-nvidia-rtx3080-eagle-oc-10gb-gddr6-display-port-hdmi/',
+    },
+	    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-pny-nvidia-geforce-rtx-3070-8gb-gddr6-uprising-displayport-1-4-x3-hdmi-2-1/',
+    },
+	{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-asus-nvidia-tuf-rtx3070-o8g-gaming-8gb-gddr6-hdmi-display-port/',
+    },
+	    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-gigabyte-aorus-nvidia-rtx3070-master-8g-8gb-gddr6-hdmi-display-port/',
+    },
+	{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-asus-nvidia-dual-rtx3070-o8g-8gb-gddr6-hdmi-display-port/',
+    },
+	    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-gigabyte-vision-nvidia-rtx3070-oc8g-8gb-gddr6-hdmi-display-port/',
+    },
+	{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-gigabyte-nvidia-rtx3070-gaming-oc-8gb-gddr6-display-port-hdmi/',
+    },
+	    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjetas-grafica-gigabyte-eagle-nvidia-rtx-3070-oc8g-8gb-gddr6-hdmi-display-port/',
+    },
+	{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-pny-nvidia-gforce-rtx-3060ti-8gb-gddr6-updrising-displayport-1-4x3-hdmi-2-1/',
+    },
+	    {
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-pny-nvidia-geforce-rtx-3060-ti-8gb-xlr8-gaming-revel-epic-x-rgb-gddr6-display-port-hdmi-doble-ventilador/',
+    },
+	{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-gigabyte-nvidia-rtx3060ti-gaming-oc-8gb-gddr6-display-port-hdmi/',
+    },
+	{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-vga-gigabyte-rx-5700-xt-gaming-oc-8gb-gddr6-3xdp-hdmi/',
+    },
+		{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-gigabyte-nvidia-rtx3060ti-eagle-oc-8gb-gddr6-display-port-hdmi/',
+    },
+		{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-asus-dual-rx-5700-xt-evo-oc-8gb-gddr6-3xdp-hdmi-8p6p/',
+    },
+		{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/tarjeta-grafica-asus-amd-dual-rx5700-o8g-evo-8gb-gddr6-hdmi-display-port/',
+    },
+			{
+      brand: 'msi',
+      model: 'ventus 3x oc',
+      series: '3080',
+      url:
+        'https://www.emeritadigital.com/producto/vga-msi-rx-5700-evoke-gp-oc-8gb-gddr6-3xdp-1xhdmi-8p6p-912-v381-090/',
+    },
+  ],
+  name: 'emerita',
+};
