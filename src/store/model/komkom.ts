8085111575cb51d902aa8603b792e9fdafbd9415
wocodlol <80688481+wocodlol@users.noreply.github.com>
2021-03-16 13:01:33 +0100

    Add files via upload

---
+++ src/store/model/komkom.ts
@@ -0,0 +1,228 @@
+import {Store} from './store';
+
+export const Komkom: Store = {
+  currency: '€',
+  labels: {
+    inStock: {
+      container: '.product-prices',
+      text: ['Disponible'],
+    },
+    outOfStock: {
+      container: '.product-prices',
+      text: ['Producto fuera de stock'],
+    },
+  },
+  links: [
+    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100091012-tarjeta-grafica-biostar-radeon-rx5700-xt.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100091796-tarjeta-grafica-gigabyte-geforce-rtx-3080-gaming-oc-10g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100090812-tarjeta-grafica-evga-geforce-rtx-3080-ftw3-ultra-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100091572-tarjeta-grafica-gigabyte-aorus-geforce-rtx-3080-xtreme-10g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092784-tarjeta-grafica-gigabyte-aorus-geforce-rtx-3080-master-10g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100091573-tarjeta-grafica-gigabyte-aorus-geforce-rtx-3080-master-10g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092596-tarjeta-grafica-evga-geforce-rtx-3080-ftw3-ultra-hybrid-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092356-tarjeta-grafica-gigabyte-geforce-rtx-3080-vision-oc-10g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092597-tarjeta-grafica-evga-geforce-rtx-3080-xc3-ultra-hybrid-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100090810-tarjeta-grafica-evga-geforce-rtx-3080-xc3-ultra-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092681-tarjeta-grafica-gigabyte-geforce-rtx-3080-eagle-oc-10g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092506-tarjeta-grafica-gigabyte-geforce-rtx-3080-eagle-10g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100090811-tarjeta-grafica-evga-geforce-rtx-3080-ftw3-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092354-tarjeta-grafica-gigabyte-aorus-geforce-rtx-3070-master-8g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100090807-tarjeta-grafica-evga-geforce-rtx-3080-xc3-black-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100090809-tarjeta-grafica-evga-geforce-rtx-3080-xc3-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100091505-tarjeta-grafica-evga-geforce-rtx-3070-ftw3-ultra-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100091795-tarjeta-grafica-gigabyte-geforce-rtx-3070-gaming-oc-8g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092943-tarjeta-grafica-gigabyte-geforce-rtx-3070-eagle-oc-8g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092504-tarjeta-grafica-gigabyte-geforce-rtx-3070-vision-oc-8g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092208-tarjeta-grafica-evga-geforce-rtx-3070-xc3-ultra-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100091587-tarjeta-grafica-evga-geforce-rtx3070-xc3-black.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100095040-tarjeta-grafica-gigabyte-geforce-rtx-3060-ti.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092585-tarjeta-grafica-gigabyte-geforce-rtx-3060-ti-gaming-oc-8g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100095039-tarjeta-grafica-gigabyte-geforce-rtx-3060-gaming-oc-12g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100095041-tarjeta-grafica-gigabyte-geforce-rtx-3060-vision-oc-12g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092586-tarjeta-grafica-gigabyte-aorus-geforce-rtx-3060-ti-master-8g.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092584-tarjeta-grafica-evga-geforce-rtx-3060-ti-ftw3-ultra-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092595-tarjeta-grafica-evga-geforce-rtx-3060-ti-xc-gaming.html',
+    },
+	    {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://komcentros.com/100092680-tarjeta-grafica-gigabyte-geforce-rtx-3060-ti-eagle-oc-8g.html',
+    },
+  ],
+  name: 'komkom',
+};
---
+++ src/store/model/opirata.ts
@@ -0,0 +1,347 @@
+import {Store} from './store';
+
+export const Opirata: Store = {
+  currency: '€',
+  labels: {
+    inStock: {
+      container: '.cart-quantity',
+      text: ['COMPRAR'],
+    },
+    outOfStock: {
+      container: '.btn btn-warning full-width white notify-me',
+      text: ['AVÍSAME'],
+    },
+  },
+  links: [
+	      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/gigabyte-vga-nvidia-rtx-3080-gaming-oc-10g-ddr6x',
+    }, 
+		      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-geforce-rtx-3070-vision-oc-8gb-gddr6',
+    }, 
+			      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-geforce-rtx-3070-gaming-oc-8gb-gddr6',
+    }, 
+			      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/gigabyte-vga-nvidia-rtx-3080-eagle-oc-10g-ddr6x',
+    }, 
+			      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-geforce-rtx-3070-eagle-oc-8gb-gddr6',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/gigabyte-aorus-vga-nvidia-rtx-3070-master-8g-ddr6',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-evga-geforce-rtx-3080-ftw3-ultra-gaming-10gb-gddr6x',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-geforce-rtx-3080-vision-oc-10gb-gddr6x',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-aorus-geforce-rtx-3080-xtreme-10gb-gddr6x',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-palit-rtx-3070-jet-stream-8gb-gddr6',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gainward-rtx-3070-phantom-gs-8gb-gddr6',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-evga-geforce-rtx-3080-xc3-black-gaming-10gb-gddr6x',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-evga-geforce-rtx-3080-ftw3-gaming-10gb-gddr6x',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-evga-geforce-rtx-3080-xc3-ultra-gaming-10gb-gddr6x',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-geforce-rtx-3080-master-10gb-gddr6x',
+    }, 
+				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-geforce-rtx-3070-eagle-8gb-gddr6',
+    }, 
+					      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/gigabyte-vga-nvidia-rtx-3060-ti-gaming-oc-pro-8gb',
+    }, 
+						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/palit-vga-nvidia-geforce-rtx-3060-ti-gamingpro-8gb-gddr6',
+    }, 
+							      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/gigabyte-vga-nvidia-rtx-3060-ti-gaming-oc-8gb-ddr6',
+    }, 
+								      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/palit-vga-nvidia-rtx-3060-ti-dual-oc-8gb-gddr6',
+    }, 
+									      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-palit-geforce-rtx-3060-ti-dual-8gb-gddr6',
+    }, 
+										      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/vga-rog-strix-rx5700xt-o8g-gaming-asus',
+    }, 
+											      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-geforce-rtx-3060-ti-eagle-oc-8gb-gddr6',
+    }, 
+												      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-powercolor-red-evil-rx-5700xt-oc-8gb-gddr6',
+    }, 
+													      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/vga-rog-strix-rx5700-o8g-ga-asus',
+    }, 
+														      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-radeon-rx-5700-xt-8gb-gddr6',
+    }, 
+															      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/vga-radeon-dual-rx5700-o8g-evo-asus',
+    }, 
+																      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-asrock-radeon-rx-5700-xt-taichi-x-8g-oc-+',
+    }, 
+																	      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-msi-rx-5700-xt-mech-oc-8gb-gddr6',
+    }, 
+																		      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-asus-radeon-rx-5700-xt-8gb-gddr6',
+    }, 
+																			      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-msi-rx-5700-xt-gaming-x-8gb-gddr6',
+    }, 
+																				      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-asus-tuf-gaming-x3-radeon-rx-5700-xt-oc-8gb-gddr6',
+    }, 
+																					      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-msi-rx-5700-xt-evoke-oc-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-powercolor-radeon-rx-5700xt-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-biostar-radeon-rx-5700xt-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-radeon-rx-5700-gaming-oc-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-sapphire-radeon-rx-5700-xt-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-msi-radeon-rx-5700-xt-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-msi-radeon-rx-5700-gaming-x-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-sapphire-radeon-rx-5700-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-msi-radeon-rx-5700-evoke-oc-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-asus-radeon-rx-5700-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-gigabyte-radeon-rx-5700-8gb-gddr6',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-msi-gaming-radeon-rx-5700-evoke-gp-oc-8gb',
+    }, 
+																						      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-msi-rx-5700-xt-evoke-oc-8gb-gddr6',
+    }, 
+																							      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-msi-rx-5700-xt-mech-oc-gp-8gb-gddr6',
+    }, 
+																							      {
+      brand: 'nvidia',
+      model: 'founders edition',
+      series: '3060ti',
+      url:
+        'https://www.opirata.com/p/tarjeta-grafica-msi-radeon-rx-5700-8gb-gddr6',
+    }, 
+  ],
+  name: 'opirata',
+};
