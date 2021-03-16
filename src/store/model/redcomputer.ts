4336c74e81050c43308602f6b84da1f0c1e73e9e
wocodlol <80688481+wocodlol@users.noreply.github.com>
2021-03-16 16:09:58 +0100

    Add files via upload

---
+++ src/store/model/redcomputer.ts
@@ -0,0 +1,36 @@
+  import {Store} from './store';
+
+export const Redcomputer: Store = {
+  currency: '€',
+  labels: {
+    inStock: {
+      container: '.available-box ',
+      text: ['En stock'],
+    },
+    outOfStock: {
+      container: '.available-box',
+      text: ['Este producto ya no está disponible'],
+    },
+  },
+  links: [
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-msi-gtx-1060-oc-3gb-978.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/msi-rtx-2060-super-ventus-oc-gp-edition-2411.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-asus-dual-rtx-3070-8gb-4126.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-asus-dual-rtx-3070-oc-8gb-4127.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-asus-rog-strix-rtx-3070-oc-8gb-gaming-4116.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-gigabyte-geforce-rtx-3080-eagle-oc-10gb-3942.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-asus-tuf-rtx-3080-oc-gaming-10gb-3944.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-asus-rog-strix-rtx-3080-oc-gaming-3946.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-gigabyte-aorus-geforce-rtx-3080-xtreme-10gb-gddr6x-4179.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-msi-radeon-rx-6800-xt-gaming-x-trio-16gb-4266.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-gigabyte-rtx-3090-eagle-oc-24gb-3940.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-gigabyte-rtx-3090-gaming-oc-24gb-3939.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-asus-tuf-rtx-3090-oc-gaming-24gb-3948.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-asus-rog-strix-geforce-rtx-3090-gaming-24gb-3949.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-asus-rog-strix-rtx-3090-oc-gaming-3950.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-asus-tuf-rtx-3090-gaming-24gb-3947.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-msi-rtx-3090-ventus-3x-24g-oc-3936.html',     },
+{       brand: 'nvidia',       model: 'founders edition',       series: '3060ti',       url:         'https://tienda.redcomputer.es/tarjeta-grafica-zotac-gaming-geforce-rtx-3090-trinity-oc-24gb-gddr6x-4423.html',     },
+	  ],
+  name: 'redcomputer',
+};
