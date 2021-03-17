d77337a91c544c225a284b110b16038a8815fdf3
Ezequiel Fernandez Burgos <ezequiel.fernandez@ext.veepee.com>
2021-03-17 12:20:24 +0100

    fix aussar

--- src/store/model/aussar.ts
+++ src/store/model/aussar.ts
@@ -18,156 +18,156 @@ export const Aussar: Store = {
   },
   links: [
 	      {
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'gainward',
+      model: 'phoenix',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/gainward-geforce-rtx-3080-phoenix.html',
     },
 	{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gainward',
+      model: 'phoenix gs',
+      series: '3090',
       url:
         'https://www.aussar.es/tarjetas-graficas/gainward-geforce-rtx-3090-phoenix-gs.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'gainward',
+      model: 'phoenix gs',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/gainward-geforce-rtx-3080-phoenix-gs.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'zotac',
+      model: 'twin edge',
+      series: '3060Ti',
       url:
         'https://www.aussar.es/tarjetas-graficas/zotac-geforce-rtx-3060-ti-twin-edge-8gb.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'palit',
+      model: 'gaming pro',
+      series: '3060Ti',
       url:
         'https://www.aussar.es/tarjetas-graficas/palit-geforce-rtx-3060-ti-dual-8gb.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'eagle',
+      series: '3060Ti',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3060-ti-eagle-8g.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'gaming oc pro',
+      series: '3060Ti',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3060-ti-gaming-pro-oc-8g.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'zotac',
+      model: 'twin edge',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/zotac-geforce-rtx-3070-twin-edge-8gb-gddr6.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'zotac',
+      model: 'twin edge',
+      series: '3060Ti',
       url:
         'https://www.aussar.es/tarjetas-graficas/zotac-geforce-rtx-3060-ti-twin-edge-8gb-oc.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'eagle',
+      series: '3060Ti',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3060-ti-eagle-oc-8g.html',
     },
 	    {
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'palit',
+      model: 'gaming pro',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/palit-geforce-rtx-3070-gamingpro.html',
     },
 	{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3060Ti',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3060-ti-gaming-oc-8g.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'asus',
+      model: 'dual oc',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/asus-geforce-rtx-3070-dual-oc-edition-8gb.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3060Ti',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3060-ti-gaming-oc-8g.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'asus',
+      model: 'dual oc',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/asus-geforce-rtx-3070-dual-oc-edition-8gb.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'palit',
+      model: 'gaming pro',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/palit-geforce-rtx-3070-jetstream-8gb.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'aorus master',
+      series: '3060Ti',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-aorus-geforce-rtx-3060-ti-master-8g.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'eagle',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-rtx-3070-eagle-oc-8gb.html',
     },
 		{
       brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      model: 'ventus 2x oc',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/msi-geforce-rtx-3070-ventus-2x-oc-8gb.html',
     },
 		{
       brand: 'msi',
       model: 'ventus 3x oc',
-      series: '3080',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/msi-geforce-rtx-3070-ventus-3x-oc-8gb.html',
     },
 	    {
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3070-gaming-oc-8gb-gddr6.html',
     },
 	{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'vision oc',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3070-vision-oc-8gb.html',
     },
@@ -179,184 +179,184 @@ export const Aussar: Store = {
         'https://www.aussar.es/tarjetas-graficas/msi-geforce-rtx-3080-ventus-3x-oc-10gb-gddr6x.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'zotac',
+      model: 'trinity',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/zotac-geforce-rtx-3080-trinity-10gb-gddr6x.html',
     },
 		{
       brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      model: 'gaming',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/msi-geforce-rtx-3070-gaming-x-trio-8gb.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'aorus master',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-rtx-3070-aorus-master-8gb-gddr6.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'zotac',
+      model: 'twin edge',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/zotac-gaming-geforce-rtx-3070-twin-edge-oc.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'zotac',
+      model: 'twin edge',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/zotac-geforce-rtx-3070-twin-edge-oc-white-edition-8gb.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'evga',
+      model: 'xc3 ultra',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/evga-geforce-rtx-3070-xc3-ultra-gaming.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'asus',
+      model: 'tuf oc',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/asus-geforce-rtx-3080-tuf-gaming-oc-10gb-gddr6x.html',
     },
 	    {
       brand: 'msi',
-      model: 'ventus 3x oc',
+      model: 'gaming x trio',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/msi-geforce-rtx-3080-gaming-x-trio-10gb-gddr6x.html',
     },
 	{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'zotac',
+      model: 'trinity oc',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/zotac-gaming-geforce-rtx-3080-trinity-oc-10gb-gddr6x.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'zotac',
+      model: 'amp holo',
+      series: '3070',
       url:
         'https://www.aussar.es/tarjetas-graficas/zotac-geforce-rtx-3070-amp-holo.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'asus',
+      model: 'strix oc',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/asus-geforce-rtx-3080-rog-strix-gaming-oc-10gb-gddr6x.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'gigabyte',
+      model: 'eagle oc',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3080-eagle-oc-10g.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'gigabyte',
+      model: 'vision oc',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3080-vision-oc-10gb.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'asus',
+      model: 'strix',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/asus-geforce-rtx-3080-rog-strix-gaming-10gb-gddr6x.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'gigabyte',
+      model: 'aorus master',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-aorus-geforce-rtx-3080-master-10gb.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'asus',
+      model: 'tuf',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/asus-geforce-rtx-3080-tuf-gaming-10gb-gddr6x.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'gigabyte',
+      model: 'gaming oc',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3080-gaming-oc-10g.html',
     },
 	    {
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'zotac',
+      model: 'amp holo',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/zotac-gaming-geforce-rtx-3080-amp-holo-10gb.html',
     },
 	{
-      brand: 'msi',
-      model: 'ventus 3x oc',
+      brand: 'palit',
+      model: 'gaming pro',
       series: '3080',
       url:
         'https://www.aussar.es/tarjetas-graficas/palit-geforce-rtx-3080-gamingpro-10g.html',
     },
 		{
       brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      model: 'gaming x trio',
+      series: '3090',
       url:
         'https://www.aussar.es/tarjetas-graficas/msi-geforcee-rtx-3090-gaming-x-trio-24gb-gddr6x.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'asus',
+      model: 'tuf',
+      series: '3090',
       url:
         'https://www.aussar.es/tarjetas-graficas/asus-geforce-rtx-3090-tuf-gaming-24gb-gddr6x.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'asus',
+      model: 'strix',
+      series: '3090',
       url:
         'https://www.aussar.es/tarjetas-graficas/asus-geforce-rtx-3090-rog-strix-gaming-24gb-gddr6x.html',
     },
 		{
       brand: 'msi',
       model: 'ventus 3x oc',
-      series: '3080',
+      series: '3090',
       url:
         'https://www.aussar.es/tarjetas-graficas/msi-geforce-rtx-3090-ventus-3x-oc-24gb-gddr6x.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'vision oc',
+      series: '3090',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-rtx-3090-vision-oc-24gb.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'eagle oc',
+      series: '3090',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3090-eagle-oc-24g.html',
     },
 		{
-      brand: 'msi',
-      model: 'ventus 3x oc',
-      series: '3080',
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3090',
       url:
         'https://www.aussar.es/tarjetas-graficas/gigabyte-geforce-rtx-3090-gaming-oc-24g.html',
     },
